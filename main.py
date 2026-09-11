fd60f7caf21c15f1e530e01dbc01a83d1d0a708a
Ornela <ornelabaldin@gmail.com>
2026-09-10 23:25:35 -0300

    Configura CORS para Vercel

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://lga-play-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
